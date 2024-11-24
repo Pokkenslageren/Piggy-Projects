1cdd842cc7f973eef2365ed34a8cccf8454bf678
Kalle-KO <kaot0001@stud.kea.dk>
2024-11-24 11:46:15 +0100

    initial commit

---
+++ src/main/java/org/example/piggyprojects/PiggyProjectsApplication.java
@@ -0,0 +1,13 @@
+package org.example.piggyprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiggyProjectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiggyProjectsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/piggyprojects/PiggyProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.piggyprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiggyProjectsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
