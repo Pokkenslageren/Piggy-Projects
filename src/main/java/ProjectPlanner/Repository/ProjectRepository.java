f6cb2101a5224997ddd811a621cb4d2b76fd18a9
JullerMus <juliusclykkegaard@gmail.com>
2024-11-27 21:42:35 +0100

    initial repo

--- src/main/java/ProjectPlanner/Repository/ProjectRepository.java
+++ src/main/java/ProjectPlanner/Repository/ProjectRepository.java
@@ -1,4 +1,35 @@
 package ProjectPlanner.Repository;
 
+import ProjectPlanner.Model.Project;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class ProjectRepository {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public ProjectRepository(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void CreateProject(Project project){
+
+    }
+
+    public Project readProject(String projectName){
+        return null;
+    }
+
+    public void updateProject(String projectName){
+
+    }
+
+    public void deleteProject(String projectName){
+
+    }
+
+
 }
