0887cd037add474d16122da4b9f767639de57b9a
MagnusHannibal <magnus7f@hotmail.com>
2024-11-29 00:11:04 +0100

    stupidfuckingmistakes man

--- src/main/java/ProjectPlanner/Service/SubprojectService.java
+++ src/main/java/ProjectPlanner/Service/SubprojectService.java
@@ -11,24 +11,47 @@ public class SubprojectService {
 
     private final SubprojectRepository subprojectRepository = new SubprojectRepository(new JdbcTemplate());
 
+    /**
+     * create subproject
+     * @param subproject
+     */
     public void createSubproject(Subproject subproject) {
         subprojectRepository.createSubproject(subproject);
     }
 
+    /**
+     * read a single subproject by id
+     * @param subprojectId
+     * @return
+     */
     public Subproject readSubproject(int subprojectId) {
         return subprojectRepository.readSubproject(subprojectId);
     }
 
+    /**
+     * reads all subproject belonging to a single projectId
+     * @param projectId
+     * @return
+     */
     public List<Subproject> readAllSubprojectsByProjectId(int projectId) {
         return  subprojectRepository.readAllSubprojectsByProjectId(projectId);
     }
 
+    /**
+     * updates subproject
+     * @param subprojectId
+     * @param subproject
+     */
     public void updateSubproject(int subprojectId, Subproject subproject) {
         subprojectRepository.updateSubproject(subprojectId, subproject);
     }
 
+    /**
+     * Deletes subproject by subprojectid
+     * @param subprojectId
+     */
     public void deleteSubproject(int subprojectId) {
         subprojectRepository.deleteSubproject(subprojectId);
     }
 
-} 
+}
