93aa6b76fb260dd88d8b9c37502bd77622ace17f
JullerMus <juliusclykkegaard@gmail.com>
2024-11-28 13:29:09 +0100

    READ, READALL, DELETE i repository og service

--- src/main/java/ProjectPlanner/Repository/ProjectRepository.java
+++ src/main/java/ProjectPlanner/Repository/ProjectRepository.java
@@ -2,8 +2,12 @@ package ProjectPlanner.Repository;
 
 import ProjectPlanner.Model.Project;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class ProjectRepository {
@@ -15,20 +19,31 @@ public class ProjectRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public void CreateProject(Project project){
+
+
+    public void createProject(){
 
     }
 
-    public Project readProject(String projectName){
-        return null;
+    public Project readProject(int projectId){
+        String query = "SELECT * FROM projects WHERE projectid = ?;";
+        RowMapper<Project> rowMapper = new BeanPropertyRowMapper<>(Project.class);
+        return jdbcTemplate.queryForObject(query,rowMapper,projectId);
     }
 
-    public void updateProject(String projectName){
+    public List<Project> readAllProjects(){
+        String query = "SELECT * FROM projets;";
+        RowMapper rowMapper = new BeanPropertyRowMapper(Project.class);
+        return jdbcTemplate.query(query, rowMapper);
+    }
+
+    public void updateProject(int projectId){
 
     }
 
-    public void deleteProject(String projectName){
-
+    public void deleteProject(int projectId){
+        String query = "DELETE FROM projects WHERE projectid = ?;";
+        jdbcTemplate.update(query,projectId);
     }
 
 
--- src/main/java/ProjectPlanner/Service/ProjectService.java
+++ src/main/java/ProjectPlanner/Service/ProjectService.java
@@ -1,4 +1,35 @@
 package ProjectPlanner.Service;
 
+import ProjectPlanner.Model.Project;
+import ProjectPlanner.Repository.ProjectRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
 public class ProjectService {
+    private final ProjectRepository projectRepository = new ProjectRepository(new JdbcTemplate()); //maybe?
+
+    public void createProject(){
+        projectRepository.createProject();
+    }
+
+    public Project readProject(int projectId){
+        return projectRepository.readProject(projectId);
+    }
+
+    public List<Project> readAllProjects(){
+        return projectRepository.readAllProjects();
+    }
+
+    public void updateProject(int projectId){
+        projectRepository.updateProject(projectId);
+    }
+
+    public void delteProject(int projectId){
+        projectRepository.deleteProject(projectId);
+    }
+
+
 }
