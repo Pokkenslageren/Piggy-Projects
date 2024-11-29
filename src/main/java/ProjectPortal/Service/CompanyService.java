c8b536c87cf60b38b7557271f5b3531858bc0ebb
KasperM2000 <kasper.madsen@busted.dk>
2024-11-29 10:59:39 +0100

    Tilføjet Service og Repository til Company og User

--- src/main/java/ProjectPortal/Service/CompanyService.java
+++ src/main/java/ProjectPortal/Service/CompanyService.java
@@ -1,4 +1,40 @@
 package ProjectPortal.Service;
 
+import ProjectPortal.Model.Company;
+import ProjectPortal.Repository.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CompanyService {
+
+    private final CompanyRepository companyRepository;
+
+    @Autowired
+    public CompanyService(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    public List<Company> readAllCompanies(int companyId, String companyName) {
+        return companyRepository.readAllCompanies();
+    }
+
+    public void createCompany(String companyName, int companyId) {
+        companyRepository.createCompany(companyName, companyId);
+    }
+
+    public Company readCompanyById(int CompanyId) {
+        return companyRepository.readCompanyById(CompanyId);
+    }
+
+    public void updateCompany(String companyName, int companyId) {
+        companyRepository.updateCompany(companyName, companyId);
+    }
+    public void deleteCompanyById(int companyId) {
+        companyRepository.deleteCompanyById(companyId);
+    }
+
 }
+
