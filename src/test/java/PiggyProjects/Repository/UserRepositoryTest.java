1ac8fb16821654d85555d7f512054e3aaa398550
KasperM2000 <kasper.madsen@busted.dk>
2024-11-29 09:38:14 +0100

    Tilføjet repository og service for Company og User

---
+++ src/test/java/PiggyProjects/Repository/UserRepositoryTest.java
@@ -0,0 +1,12 @@
+package PiggyProjects.Repository;
+
+import PiggyProjects.Model.User;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRepositoryTest {
+    UserRepository userRepository;
+    User testUser;
+
+
+}
