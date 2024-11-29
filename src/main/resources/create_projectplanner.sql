6bc3c8903854c42189eec883ceaefd38e7897869
JullerMus <juliusclykkegaard@gmail.com>
2024-11-29 09:33:42 +0100

    start sql script

---
+++ src/main/resources/create_projectplanner.sql
@@ -0,0 +1,21 @@
+-- DROP DATABASE project_planner;
+
+use project_planner;
+
+CREATE TABLE if not exists `companies`(
+    company_id int NOT NULL AUTO_INCREMENT,
+    company_name varchar(255),
+    PRIMARY KEY (company_id)
+);
+
+
+
+CREATE TABLE if not exists `users`(
+    
+)
+
+CREATE TABLE if not exists projects
+
+CREATE TABLE if not exists subprojects
+
+CREATE TABLE if not exists tasks
