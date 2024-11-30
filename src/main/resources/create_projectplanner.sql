0c318e5aa973e1ddf6b3da55b2335dc9befed5cd
JullerMus <45451701+JullerMus@users.noreply.github.com>
2024-11-30 10:48:48 +0100

    Update create_projectplanner.sql
    Create_projectplanner.sql done.

--- src/main/resources/create_projectplanner.sql
+++ src/main/resources/create_projectplanner.sql
@@ -1,21 +1,61 @@
 -- DROP DATABASE project_planner;
 
+CREATE DATABASE if not exists `project_planner`;
+
 use project_planner;
 
 CREATE TABLE if not exists `companies`(
     company_id int NOT NULL AUTO_INCREMENT,
     company_name varchar(255),
-    PRIMARY KEY (company_id)
+    PRIMARY KEY (`company_id`)
 );
 
-
-
 CREATE TABLE if not exists `users`(
-    
-)
+    user_id int NOT NULL AUTO_INCREMENT,
+    company_id int DEFAULT NULL,
+    user_name varchar(255),
+    user_password varchar(64) NOT NULL,
+    PRIMARY KEY (`user_id`),
+    FOREIGN KEY (`company_id`) REFERENCES `companies` (`company_id`)
+);
 
-CREATE TABLE if not exists projects
+CREATE TABLE if not exists `projects`(
+    project_id int NOT NULL AUTO_INCREMENT,
+    company_id int DEFAULT NULL,
+    project_name varchar(255),
+    start_date date NOT NULL,
+    end_date date,
+    total_estimated_cost double,
+    total_estimated_employees int,
+    is_complete boolean,
+    project_description text,
+    PRIMARY KEY (`project_id`),
+    FOREIGN KEY (`company_id`) REFERENCES `companies` (`company_id`)
+);
 
-CREATE TABLE if not exists subprojects
+CREATE TABLE if not exists `subprojects`(
+    subproject_id int NOT NULL AUTO_INCREMENT,
+    project_id int DEFAULT NULL,
+    start_date date NOT NULL,
+    end_date date,
+    total_estimated_cost double,
+    total_available_employees int,
+    is_complete boolean,
+    subproject_description text,
+    PRIMARY KEY (`subproject_id`),
+    FOREIGN KEY (`project_id`) REFERENCES `projects` (`project_id`)
+);
 
-CREATE TABLE if not exists tasks
+CREATE TABLE if not exists `tasks`(
+    task_id int NOT NULL AUTO_INCREMENT,
+    subproject_id int DEFAULT NULL,
+    task_name varchar(255),
+    start_date date,
+    end_date date,
+    estimated_cost double,
+    assigned_employees int,
+    is_complete boolean,
+    task_description text,
+    PRIMARY KEY (`task_id`),
+    FOREIGN KEY (`subproject_id`) REFERENCES `subprojects` (`subproject_id`)
+);
