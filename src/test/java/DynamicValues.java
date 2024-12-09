ca13ed2b6f3d5331e609f0873c0d0d1bf7dd746e
JullerMus <juliusclykkegaard@gmail.com>
2024-12-09 16:33:00 +0100

    testing functionality

---
+++ src/test/java/DynamicValues.java
@@ -0,0 +1,21 @@
+import ProjectPortal.Model.Priority;
+import ProjectPortal.Model.Task;
+
+import java.time.*;
+
+public class DynamicValues {
+
+
+
+/*    LocalDate startDate = LocalDate.of(2024, 12,5);
+    LocalDate endDate = LocalDate.of(2024, 12, 15);*/
+
+
+    public int totalTaskHours(Task task){
+
+        int numberOfEmployees = 10;
+        return (task.getEndDate().getDayOfYear() - task.getStartDate().getDayOfYear()) * numberOfEmployees * 8;
+    }
+
+
+}
---
+++ src/test/java/DynamicValuesTest.java
@@ -0,0 +1,22 @@
+import ProjectPortal.Model.Priority;
+import ProjectPortal.Model.Task;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DynamicValuesTest {
+
+    DynamicValues dynamicValues = new DynamicValues();
+
+    Task task = new Task(1, 2, 1, "testingTask",10, 25000.0, LocalDate.of(12,10,5), LocalDate.of(12,10,15), false, "a task for testing functionality", Priority.valueOf("HIGH"),500);
+
+
+
+    @Test
+    void totalTaskHours() {
+
+        assertEquals(800,dynamicValues.totalTaskHours(task));
+    }
+}
