a684726129a8a8773b66c8390b7aecadf89e68da
MagnusHannibal <magnus7f@hotmail.com>
2024-12-15 19:08:03 +0100

    sidebar and topnav create-task.html. task controller changed

--- src/main/java/ProjectPortal/Controller/TaskController.java
+++ src/main/java/ProjectPortal/Controller/TaskController.java
@@ -1,9 +1,12 @@
 package ProjectPortal.Controller;
 
+import ProjectPortal.Model.Project;
 import ProjectPortal.Model.Task;
 import ProjectPortal.Model.User;
+import ProjectPortal.Service.ProjectService;
 import ProjectPortal.Service.TaskService;
 import ProjectPortal.Service.UserService;
+import ProjectPortal.Service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,25 +21,31 @@ public class TaskController {
 
     private final TaskService taskService;
     private final UserService userService;
+    private final ProjectService projectService;
 
-    public TaskController(TaskService taskService, UserService userService) {
+    public TaskController(TaskService taskService, UserService userService, ProjectService projectService) {
         this.taskService = taskService;
         this.userService = userService;
+        this.projectService = projectService;
     }
 
-    @GetMapping("/{userId}/portfolio/{projectId}/{subprojectId}/createtask")
-    public String createTask(@PathVariable("userId") int userId,@PathVariable("projectId") int projectId, @PathVariable("subprojectId") int subprojectId, Model model) {
+    @GetMapping("/{userId}/portfolio/{projectId}/createtask")
+    public String createTask(@PathVariable("userId") int userId, @PathVariable("projectId") int projectId, Model model) {
         User user = userService.readUserById(userId);
+        Project project = projectService.readProject(projectId);  // Get project info
         Task task = new Task();
-        task.setSubprojectId(subprojectId);
+        task.setProjectId(projectId);
+        task.setIsComplete(false);
+
         model.addAttribute("task", task);
+        model.addAttribute("project", project);  // Need project for navigation
         return "create-task";
     }
 
-    @PostMapping("/{userId}/portfolio/createtask")
-    public String createTask(@PathVariable("userId") int userId, @ModelAttribute Task task){
+    @PostMapping("/{userId}/portfolio/{projectId}/createtask")
+    public String createTask(@PathVariable("userId") int userId, @PathVariable("projectId") int projectId, @ModelAttribute Task task) {
         taskService.createTask(task);
-        return "redirect:/portfolio";
+        return "redirect:/" + userId + "/portfolio/" + projectId;  // Redirect back to project view
     }
 
 }
